ec669b9484606a314b5e0bc4f227ab2520038414
Alex Kern <alexkern@Alexs-MacBook-Pro-3.local>
2018-05-19 22:02:36 -0700

    Updated how I wrote it

--- src/CodeFightSols.java
+++ src/CodeFightSols.java
@@ -2,7 +2,16 @@ public class CodeFightSols {
 
 
     public static void main(String[] args) {
-        System.out.println(weakNumbers(5));
+        //System.out.println(weakNumbers(5));
+        int max = 0;
+        int n = 1000;
+        for (int i = 1; i < n; i++) {
+            //int[] a = weakNumbers(i);
+            //System.out.println("i: "  + i + "  Divisors:"  + countDivisors(i));
+            max = Math.max(max, countDivisors(i));
+        }
+        System.out.println(max);
+        System.out.println(Math.sqrt(n));
     }
     /**
      * We define the weakness of number x as the number of positive integers smaller than x that have more divisors than x.
@@ -45,7 +54,10 @@ public class CodeFightSols {
      Array of two elements: the weakness of the weakest number, and the number of integers in range [1, n] with this weakness.
      */
     /**
-     * found This puzzle particularly satisfying,
+     * found This puzzle particularly satisfying, expected input was <= 1000
+     * The runtime if it were larger woould be similar to O(nlogn),
+     * as the size of the largest possible divisor makes the counting sort
+     * when x is 1,000,000 roughly 350.
      * @param n looking for weakest number related to n
      * @return
      */
@@ -56,13 +68,18 @@ public class CodeFightSols {
         }
         int max = 0;
         int sum = 0;
-        int weakest = 0;
 
+        // This counter stores the max values for every weak number i
+        // Radix was a bad name for the second array, however it helps
+        // keep track of each number
         int[] counter = new int[n + 1];
         int[] radix = new int[50];
+        // I suspect this can be optimized using prime numbers, reducing
+        // the entire method to a O(n) time, but I found quickly
+        // I did not have time to play with the math.
         for (int i = 1; i <= n; i++) {
             int temp = countDivisors(i);
-
+            // note radix is more of a counting method, which is why it is a bad name
             radix[temp]++;
             for (int j = temp + 1; j < radix.length; j++) {
                 sum += radix[j];
@@ -73,15 +90,21 @@ public class CodeFightSols {
             }
             sum = 0;
         }
+        // run through the counter and find each.
         for (int i : counter) {
             if (i == max) {
                 sum++;
             }
         }
         int[] sol = {max, sum};
-        if (n == 9) sol[1] = 2;
         return sol;
     }
+
+    /**
+     * I did not write this, credit goes to @https://www.geeksforgeeks.org/count-divisors-n-on13/
+     * @param n number to find divisors
+     * @return number of divisors (must be greater than or equal to zero)
+     */
     private static int countDivisors(int n)
     {
         int cnt = 0;
